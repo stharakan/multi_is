d96a3d36d3d04bafb161ecfce5e0729153171e37
Sameer Tharakan <sameer@bellman.ices.utexas.edu>
2017-12-06 17:03:02 -0600

    added knn_regression file

---
+++ matlab/feature_selection/NNDistanceScaleToRank.m
@@ -0,0 +1,20 @@
+function [ nn_dist_scaled ] = NNDistanceScaleToRank( nn_dist,bw_rank,bw_scale )
+%NNDISTANCESCALETORANK scales an nn x kk matrix nn_dist of distance to
+%nearest neighbors by dividing by a variable bandwidth. The bandwidth is
+%selected by using the distance given by the bw_rank^th neighbor distance.
+%This can be additionally scaled by the thrid arg bw_scale. Default
+%bw_scale = 1.
+
+if nargin == 2
+    bw_scale = 1;
+end
+
+% get variable bws
+bws = nn_dist(:,bw_rank).*bw_scale;
+
+% set up exponential matrix
+nn_dist_scaled = bsxfun(@rdivide, nn_dist,bws);
+
+
+end
+
