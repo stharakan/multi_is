fc610dad6c14916a3b543b1dee0cb5dfda91d94c
Sameer Tharakan <sameer@bellman.ices.utexas.edu>
2018-01-18 13:12:31 -0600

    updated to remove epsilon errors

--- matlab/patch_selection/AveragePatchValues.m
+++ matlab/patch_selection/AveragePatchValues.m
@@ -5,17 +5,23 @@ if nargin < 3
 end
 
 % create filter matx
-filt = ones(psize)./(psize^2);
+p2 = psize * psize;
+filt = ones(psize)./(p2);
 
 % filter image (assume at most 3d)
-avg_im = zeros(size(im));
-d3 = size(im,3);
-for di = 1:d3
-	avg_im(:,:,di) = imfilter(im(:,:,di),filt);
-end
+%avg_im = zeros(size(im));
+%d3 = size(im,3);
+%for di = 1:d3
+%	avg_im(:,:,di) = imfilter(im(:,:,di),filt);
+%end
+avg_im = imfilter(im,filt);
 
 % extract idx
 avgs = avg_im(idx);
 
+% round?
+avgs = avgs.*(p2);
+avgs = round(avgs);
+avgs = avgs./p2;
 
 end
