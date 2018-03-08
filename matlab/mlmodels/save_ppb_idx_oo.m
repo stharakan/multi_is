fa4fde57f76124eba40e283e75daacc3e5022d40
Sameer Tharakan <tharakan@login3.stampede2.tacc.utexas.edu>
2018-03-08 15:52:59 -0600

    updated obj ori save idx

--- matlab/mlmodels/save_ppb_idx_oo.m
+++ matlab/mlmodels/save_ppb_idx_oo.m
@@ -1,14 +1,63 @@
 % Add path to other code
-bdir = [getenv('HOME'),'/Documents/data/trainingdata/'];
-addpath([getenv('HOME'),'/Documents/data/trainingdata/']);
+addpath(genpath('./../'))
+prdir = [getenv('PRDIR'),'/'];
+prsdir = [getenv('PRDIRSCRATCH'),'/'];
+bdir = [getenv('BRATSDIR'),'/preprocessed/trainingdata/meanrenorm/'];
+%bdir = [getenv('BRATSDIR'),'/preprocessed/trainingdata/HGG/pre-norm-aff/'];
+addpath(bdir);
+bcell = BrainCellAllTrain();
 
 % initialize brainlist, ps
-%ps = PointSelector('neartumor');
-%ps = PointSelector('edemanormal');
-ps = PointSelector('all');
-blist = BrainPointList(bdir,[],ps,0,'./');
+num_brains = 260;
+split_perc = 0.8;
+ppb = 200;
+bperc = 0.02;
+psize = 9;
+ftype = 'patchstats';
+target = 2;
+params = [];
+outdir = '';
+%ps = PointSelector('neartumor',ppb);
+%ps = PointSelector('edemanormal',ppb);
+%ps = PointSelector('all',ppb);
+ps = PointSelector('edemadist',bperc,psize);
 
-AnalyzePatchProbabilities(blist,5,2,'./');
+if BrainPointList.CheckForList(prsdir,ps,num_brains)
+    disp('Loading blist');
+    blist = BrainPointList.LoadList(prsdir,ps,num_brains);
+else
+    disp('Making blist')
+    blist = BrainPointList(bdir,bcell,ps,prsdir);
+end
+blist.PrintListInfo();
+
+disp('Splitting into train and test..');
+[trn,tst] = blist.Split(split_perc); % TODO add rounding func
+trn.SaveList(prsdir);
+tst.SaveList(prsdir);
+disp('Lists saved!');
+
+
+disp('Computing training features..')
+[ ~,fcell ] = GetBlistPatchFeatureData( trn,psize,ftype,prsdir );
+
+disp('Computing testing features..')
+[ ~,fcell ] = GetBlistPatchFeatureData( tst,psize,ftype,prsdir );
+disp('Features computed!');
+
+
+%AnalyzePatchProbabilities(blist,5,2,'./');
+
+%disp('Running feature regression');
+%[ franks ] = FeatureRankerRegression( blist,ftype,psize,target,params,prsdir );
+%
+%disp('Feature ranks');
+%disp('');
+%fcell(franks)
+
+
+%AnalyzePatchProbabilities(blist,17,2,prsdir);
+%AnalyzePatchProbabilities(blist,17,2,prsdir);
 
 
 
