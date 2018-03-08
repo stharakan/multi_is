78d63d3cae5574a279431b6aa53dfdfc32073052
Sameer Tharakan <tharakan@login3.stampede2.tacc.utexas.edu>
2018-03-08 16:07:33 -0600

    added script to be called from bash

--- matlab/knn/PreprocessForKNN.m
+++ matlab/knn/PreprocessForKNN.m
@@ -1,83 +1,83 @@
-function [] = PreprocessForKNN(patch_sizes,ftypes,pstr)
+function [] = PreprocessForKNN(bdir,outdir,fkeep,stype,psize,ftype,pstr,ppb,target)
+%function [] = PreprocessForKNN(patch_sizes,ftypes,pstr)
 
 % initialize loop stuff
-prdir = [getenv('PRDIR'),'/']; 
-prsdir = [getenv('PRDIRSCRATCH'),'/']; 
-addpath(genpath( [getenv('MISDIR'),'/matlab/']));
+if isempty(outdir)
+  outdir = [getenv('PRDIRSCRATCH'),'/'];
+end
+if isempty(bdir)
+  bdir = [getenv('BRATSDIR'),'/preprocessed/trainingdata/meanrenorm/'];
+end
+if isempty(stype)
+  stype = 'reg01';
+end
 ntrn = 208; ntst = 52;
 
-for pi = 1:length(patch_sizes)
-  psize = patch_sizes(pi);
 % initialize ps
-switch pstr
-  case 'nearedema'
-    ppb = 4000;
-    ps = PointSelector('nearedema',ppb);
-  case 'edemanormal'
-    ppb = 4000;
-    ps = PointSelector('edemanormal',ppb);
-  case 'edemadist'
-    bperc = 0.02;
-    ps = PointSelector('edemadist',bperc,psize);
+if strcmp(pstr,'edemadist')
+  ps = PointSelector(pstr,ppb,psize);
+else
+  ps = PointSelector(pstr,ppb);
 end
 fprintf('Point selector: %s\n',ps.PrintString());
 
-
+% load lists
 disp('Loading train and test lists..');
-trn = BrainPointList.LoadList(prsdir,ps,ntrn);
-%trn.PrintListInfo();
-tst = BrainPointList.LoadList(prsdir,ps,ntst);
-%tst.PrintListInfo();
+trn = BrainPointList.LoadList(outdir,ps,ntrn);
+trn.PrintListInfo();
+tst = BrainPointList.LoadList(outdir,ps,ntst);
+tst.PrintListInfo();
 
 
-% loop over sfbs
-for fi = 1:length(ftypes)
-ftype = ftypes{fi};
-  fprintf('Processing psize %d for ftype %s\n',psize,ftype);
-  
-  % load training data (single prec)
-  fprintf(' Loading training  ..\n');
-  [ Gsingle,~ ] = GetBlistPatchFeatureData( trn,psize,ftype,prsdir );
+% load training data (single prec)
+fprintf(' Loading training data and franks ..\n');
+[ Gsingle,~ ] = GetBlistPatchFeatureData( trn,psize,ftype,outdir );
+dd = size(Gsingle,2);
+fkeep= min(fkeep,dd);
 
-  % get means, stds
-  fprintf(' Computing training means/stds ..\n');
-  means = mean(Gsingle);
-  stds = std(Gsingle);
-  
-  % apply to training data
-  Gsingle = bsxfun(@minus,Gsingle,means);
-  Gsingle = bsxfun(@rdivide,Gsingle,stds);
-  Gdouble = double(Gsingle');
-  clear Gsingle
-  
-  % save new training
-  sfile = trn.MakeFeatureDataString(ftype,psize);
-  sfile = [prsdir,'knntrn',sfile];
-  fprintf(' Saving training to file %s ..\n',sfile);
-  fid = fopen(sfile,'w');
-  fwrite(fid,Gdouble,'double');
-  fclose(fid);
-  clear Gdouble
+% load franks, truncate to fkeep
+[~,ftOrder,AvgPos] = LoadFtRanks(trn,stype,ftype,psize,target,outdir);
+Gsingle = Gsingle(:,ftOrder(1:fkeep));
 
-  % Load testing 
-  fprintf(' Loading testing  ..\n');
-  [ Gsingle,~ ] = GetBlistPatchFeatureData( tst,psize,ftype,prsdir );
+% get means, stds
+fprintf(' Computing training means/stds ..\n');
+means = mean(Gsingle);
+stds = std(Gsingle);
 
-  % apply means/stds to testing
-  Gsingle = bsxfun(@minus,Gsingle,means);
-  Gsingle = bsxfun(@rdivide,Gsingle,stds);
-  Gdouble = double(Gsingle');
-  clear Gsingle
+% apply to training data
+Gsingle = bsxfun(@minus,Gsingle,means);
+Gsingle = bsxfun(@rdivide,Gsingle,stds);
+Gdouble = double(Gsingle');
+clear Gsingle
 
-  % save testing
-  sfile = tst.MakeFeatureDataString(ftype,psize);
-  sfile = [prsdir,'knntst',sfile];
-  fprintf('Saving testing to file %s ..\n',sfile);
-  fid = fopen(sfile,'w');
-  fwrite(fid,Gdouble,'double');
-  fclose(fid);
-  clear Gdouble
+% save new training
+sfile = trn.MakeFeatureDataString(ftype,psize);
+sfile = [outdir,'knntrn.dd.',num2str(fkeep),'.',sfile];
+fprintf(' Saving training to file %s ..\n',sfile);
+fid = fopen(sfile,'w');
+fwrite(fid,Gdouble,'double');
+fclose(fid);
+clear Gdouble
 
-end
+% Load testing , truncate
+fprintf(' Loading testing  ..\n');
+[ Gsingle,~ ] = GetBlistPatchFeatureData( tst,psize,ftype,outdir );
+Gsingle = Gsingle(:,ftOrder(1:fkeep));
+
+% apply means/stds to testing
+Gsingle = bsxfun(@minus,Gsingle,means);
+Gsingle = bsxfun(@rdivide,Gsingle,stds);
+Gdouble = double(Gsingle');
+clear Gsingle
+
+% save testing
+sfile = tst.MakeFeatureDataString(ftype,psize);
+sfile = [outdir,'knntst.dd.',num2str(fkeep),'.',sfile];
+fprintf('Saving testing to file %s ..\n',sfile);
+fid = fopen(sfile,'w');
+fwrite(fid,Gdouble,'double');
+fclose(fid);
+clear Gdouble
+
 
 end
