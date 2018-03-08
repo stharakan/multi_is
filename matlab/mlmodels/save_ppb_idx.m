2f0c2792599048384ca4a551d1fd4cc26a8ebc6f
Sameer Tharakan <tharakan@login3.stampede2.tacc.utexas.edu>
2018-03-08 15:51:59 -0600

    added save ppb idx new

--- matlab/mlmodels/save_ppb_idx.m
+++ matlab/mlmodels/save_ppb_idx.m
@@ -1,29 +1,46 @@
 % Add path to other code
-addpath([getenv('BRATSREPO'),'/matlab/general/']);
+addpath([getenv('MISDIR'),'/matlab/general/']);
+brats = getenv('BRATSDIR');
 SetPath;
 SetVariablesTACC;
 
 % params
 nt_frac = 0.25;
+max_tumor_pts_allowed = 100
 
 % merge brns
-sv_base = ['BRATS_50M_Meta_agg'];
-idxfile = [brats,'/classification/',sv_base,'.idxs.mat'];
-
+sv_base = ['BRATS.nt',num2str(max_tumor_pts_allowed)];
+idxfile = [brats,'/classification/training/models/',sv_base,'.idxs.mat'];
+brn_dir = [brats,'/preprocessed/trainingdata/meanrenorm/'];
 fprintf('Testing save to %s\n',idxfile)
 
 % Location of brains
 lgg_dir = [brats,'/preprocessed/trainingdata/LGG/pre-norm-aff/'];
 lggcell = GetBrnList(lgg_dir);
 addpath(lgg_dir);
-if 0
 tic;
 disp('LGG')
 for bi = 1:length(lggcell)
+        brain = lggcell{bi};
 	fprintf('Processing brain %d out of %d: %s ',bi,length(lggcell),lggcell{bi});
-  	[ flair,~,~,~,seg ] = ReadIdxBratsBrain( lgg_dir,lggcell{bi} );
+  	[ flair,~,~,~,seg ] = ReadIdxBratsBrain( brn_dir,lggcell{bi} );
 	ridx = find(seg(:));	
 	num_tp = length(ridx);
+	
+	if strcmp(brain, 'Brats17_2013_10_1')
+		fprintf('Brain %s is being set to 0\n',brain)
+ 		num_tp = 0;
+		ridx = [];
+		ridxc_hgg{bi} = ridx;
+		continue;
+	end
+        
+	if num_tp > max_tumor_pts_allowed
+        ridx_sel = randperm(num_tp,max_tumor_pts_allowed);
+        ridx = ridx(ridx_sel);
+        num_tp = max_tumor_pts_allowed;
+        end
+	
 	allowed_idx = find(flair);
 	num_ntp = round(nt_frac*num_tp);
 	num_fp = num_tp - num_ntp;
@@ -33,9 +50,11 @@ for bi = 1:length(lggcell)
 	[ridx2,num_fp] = FarTumorIdx(num_fp,seg,flair);
 	fprintf('.');
 	[ridx3] = ridx;
+	
 
 	ridx = [ridx1(:);ridx2(:);ridx3(:)];
 	ridxc_lgg{bi} = ridx;
+
 	fprintf(' done!\n');
 	fprintf('Num tumor pixels: %d\n',num_tp);
 end
@@ -50,10 +69,28 @@ addpath(hgg_dir);
 tic;
 disp('HGG');
 for bi = 1:length(hggcell)
+        brain = hggcell{bi};
+	
+	if strcmp(brain, 'Brats17_2013_10_1')
+		fprintf('Brain %s is being set to 0\n',brain)
+ 		num_tp = 0;
+		ridx = [];
+		ridxc_hgg{bi} = ridx;
+		continue;
+	end
+
+
 	fprintf('Processing brain %d out of %d: %s ',bi,length(hggcell),hggcell{bi});
-  	[ flair,~,~,~,seg ] = ReadIdxBratsBrain( hgg_dir,hggcell{bi} );
+  	[ flair,~,~,~,seg ] = ReadIdxBratsBrain( brn_dir,hggcell{bi} );
 	ridx = find(seg(:));	
 	num_tp = length(ridx);
+        
+	if num_tp > max_tumor_pts_allowed
+        ridx_sel = randperm(num_tp,max_tumor_pts_allowed);
+        ridx = ridx(ridx_sel);
+        num_tp = max_tumor_pts_allowed;
+        end
+	
 	allowed_idx = find(flair);
 	num_ntp = round(nt_frac*num_tp);
 	num_fp = num_tp - num_ntp;
@@ -73,9 +110,9 @@ toc
 % save indices
 save(idxfile,'ridxc_lgg','ridxc_hgg','lggcell','hggcell');
 
-end
 
 % all gg
+if 0
 agg_dir = [brats,'/preprocessed/trainingdata/all-pre-norm-aff/'];
 brncell = GetBrnList(agg_dir);
 addpath(agg_dir);
@@ -86,6 +123,15 @@ for bi = 1:length(brncell)
   	[ flair,~,~,~,seg ] = ReadIdxBratsBrain( agg_dir,brncell{bi} );
 	ridx = find(seg(:));	
 	num_tp = length(ridx);
+
+	% adjust num_tp for max tumor points per brain
+        if num_tp > max_tumor_pts_allowed
+        ridx_sel = randperm(num_tp,max_tumor_pts_allowed);
+        ridx = ridx(ridx_sel);
+        num_tp = max_tumor_pts_allowed;
+        end
+
+
 	allowed_idx = find(flair);
 	num_ntp = round(nt_frac*num_tp);
 	num_fp = num_tp - num_ntp;
@@ -103,3 +149,4 @@ for bi = 1:length(brncell)
 end
 
 save(idxfile,'ridxc','brncell');
+end
