513f0b154914608fa4facea3c6cc5587faa78d5e
Sameer Tharakan <sameer@ices.utexas.edu>
2018-04-17 16:35:26 -0500

    added funcs to brain reader class for writing data

--- matlab/readbrns/BrainReader.m
+++ matlab/readbrns/BrainReader.m
@@ -13,7 +13,7 @@ classdef BrainReader
             obj.bname = b_name;
         end
         
-        % Reader files
+        % Reader funcs
         function im = ReadFlair(obj)
             fbase = [obj.bdir,obj.bname,'/',obj.bname];
             nifti = load_untouch_nii([fbase,'_flair_normaff.nii.gz']);
@@ -58,6 +58,32 @@ classdef BrainReader
             t2 = obj.ReadT2();
             seg = obj.ReadSeg();
         end
+        
+        function P = ReadProbs(obj,pdir,pstr)
+            fname = [pdir,obj.MakeDataString(pstr,'nii.gz')];
+            nifti = load_untouch_nii(fname);
+            P = nifti.img;
+        end
+        
+        % writing funcs
+        function st = MakeDataString(obj,prefix,suffix)
+            st = [prefix,'.',obj.bname,'.',suffix];
+        end
+        
+        function [] = SaveProbs(obj,P,pdir,pstr)
+            fname = [pdir,obj.MakeDataString(pstr,'nii.gz')];
+            nifti = make_nii(P);
+            nifti.untouch = 1;
+            nifti.hdr = obj.GetSingleHdr();
+            save_untouch_nii(nifti,fname);
+        end
+            
+        function hdr = GetSingleHdr(obj)
+            fbase = [obj.bdir,obj.bname,'/',obj.bname];
+            nifti = load_untouch_nii([fbase,'_flair_normaff.nii.gz']);
+            hdr = nifti.hdr;
+        end
+            
         
     end
     
