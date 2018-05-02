511bf6a5253b59ae664e3cb95b47ffcd2f21047c
Sameer Tharakan <tharakan@login3.maverick.tacc.utexas.edu>
2018-05-02 12:08:42 -0500

    added bash script for bw reg lookup

---
+++ matlab/runfiles.s2/BwRegLookup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo $FTYPE
+echo $PSIZE
+
+if [ "$FTYPE" == "patchgabor" ]; then
+  if [ "$PSIZE" == "5" ]; then
+    BW=1.47;
+  elif [ ${PSIZE} = 9 ]; then
+    BW=1.16;
+  elif [ ${PSIZE} = 17 ]; then
+    BW=1.18;
+  else
+    echo "Patch size not recognized! Assigning bw = 1.0"
+    BW=1.0
+  fi
+elif [ "$FTYPE" == "patchstats" ]; then
+  if [ ${PSIZE} = 5 ]; then
+    BW=1.99;
+  elif [ ${PSIZE} = 9 ]; then
+    BW=1.91;
+  elif [ ${PSIZE} = 17 ]; then
+    BW=1.59;
+  else
+    echo "Patch size not recognized! Assigning bw = 1.0"
+    BW=1.0
+  fi
+elif [ "$FTYPE" == "patchgstats" ]; then
+  if [ ${PSIZE} = 5 ]; then
+    BW=0.63;
+  elif [ ${PSIZE} = 9 ]; then
+    BW=1.31;
+  elif [ ${PSIZE} = 17 ]; then
+    BW=1.81;
+  else
+    echo "Patch size not recognized! Assigning bw = 1.0"
+    BW=1.0
+  fi
+else
+  echo "Feature type not recognized! Assigning bw = 1.0"
+  BW=1.0
+fi
