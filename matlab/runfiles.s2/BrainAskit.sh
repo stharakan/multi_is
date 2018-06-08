b33a70beb737ea7393537e3dfe4aecfb23115392
Sameer Tharakan <tharakan@login2.stampede2.tacc.utexas.edu>
2018-06-08 11:25:46 -0500

    changed to test eval, fixed bugs, added dependency info

--- matlab/runfiles.s2/BrainAskit.sh
+++ matlab/runfiles.s2/BrainAskit.sh
@@ -4,18 +4,20 @@ source ./Parser.sh
 filen=askb.${BW}.${FKEEP}.${TARGET}.${FTYPE}.${PSSTR}.${PSP1}.${PSIZE}
 
 # set askit running params 
-mm=256
-id_rank=128
+mm=64
+id_rank=32
 id_tol=0.00000001
 
 # tstdata
-tstfile=$(ls ${OUTDIR}/knntst*${FKEEP}*${FTYPE}*${PSIZE}*${BRAIN}*bin)
-IFS='.' read -ra delims <<< ${tstfile}
-n_tst=${delims[-2]};
+#tstfile=$(ls ${OUTDIR}/knntst*${FKEEP}*${FTYPE}*${PSIZE}*${BRAIN}*bin) # Know this?
+#IFS='.' read -ra delims <<< ${tstfile}
+#n_tst=${delims[-2]};
+n_tst=8928000
+tstfile=${OUTDIR}/knntst.dd.${FKEEP}.${FTYPE}.ps.${PSIZE}.${BRAIN}.nn.${n_tst}.bin;
 tknnfile=${tstfile%.bin}.kk.${KCUT}.bin;
 tknnfile=${tknnfile/knntst/nntstlist};
 
-# ASSUME CORRECT BW is passed!!!
+# ASSUME CORRECT BW is passed!!! 
 #mdlfile=$(ls ${OUTDIR}/askitregmdl.dd.${FKEEP}*data*${FTYPE}*ps.${PSIZE}.${PSSTR}*${PSP1}*)
 trnfile=$(ls ${OUTDIR}/knntrn.dd.${FKEEP}*data*${FTYPE}*ps.${PSIZE}.${PSSTR}*${PSP1}*)
 IFS='.' read -ra delims <<< ${trnfile}
@@ -36,15 +38,20 @@ onesfile=${potfile/ppv/${BRAIN}.${FTYPE}.1s};
 cmfile=${potfile/ppv/${BRAIN}.${FTYPE}.cm};
 rofile=${potfile/ppv/${BRAIN}.${FTYPE}.ro};
 
+# sourcefile
+srcfile=${MISDIR}/matlab/askit/sourceme.s2.knl.askit
+source ${srcfile}
+
+
 # Make sbatch runfile
 echo "#!/bin/bash
 #SBATCH -J ${filen} # Job name
 #SBATCH -o ${filen}.o # Name of stdout output file
 #SBATCH -e ${filen}.e # Name of stderr error file
-#SBATCH -p skx-normal # Queue name
+#SBATCH -p normal # Queue name
 #SBATCH -N 32 # Total # of nodes (now required)
 #SBATCH -n 32 # Total # of mpi tasks
-#SBATCH -t 2:20:00 # Run time (hh:mm:ss)
+#SBATCH -t 2:00:00 # Run time (hh:mm:ss)
 #SBATCH --mail-user=sameer@ices.utexas.edu
 ##SBATCH --mail-type=begin # Send email at begin and end of job
 #SBATCH --mail-type=end # Send email at begin and end of job
@@ -53,44 +60,45 @@ echo "#!/bin/bash
 # Run trn/tst list creation on skx s2 nodes
 echo $(date) Begin
 pwd
-export OMP_NUM_THREADS=48
-export KS_IC_NT=48
-export GSKNN_IC_NT=48
 
 cd $MISDIR/matlab
 
-source askit/sourceme.s2.askit
+#source askit/sourceme.s2.knl.askit
+source ${srcfile} 
 
-time ibrun tacc-affinity /home1/02497/jll2752/lib/askit_skx/treecode/src/askit_distributed_main.exe \
+cd $PRDIRSCRATCH/coredump
+ulimit -c unlimited
+
+time ibrun tacc-affinity ${ASKIT_DIR}/src/askit_distributed_main.exe \
 -data ${trnfile} \
 -knn_file ${knnfile} \
 -charges ones -N ${n_ref} -d ${FKEEP} -k ${KCUT} -m ${mm} -id_rank ${id_rank} -min_skeleton_level 7 -id_tol ${id_tol} \
 -sort_method nn -kernel_type gaussian -balloon_k 0 -h ${BW} -c 0 -p 0 -output output.out \
--training_potentials_file None \
 -test_potentials_file ${onesfile} -num_power_iterations 10 \
 -test_data_file ${tstfile} \
 -test_knn_file ${tknnfile} \
 -num_test_points ${n_tst} -num_skel_targets 2 -num_uniform_required 0 -oversampling_fac 5 -err 100 \
 -num_error_repeats 10 -max_sampling_iterations 3 -adaptive_sample_size 2 -pruning_num_neighbors 0 \
 -neighbors_to_pass_up 4 -lambda 0.0 -binary -do_simplified_adaptive_rank \
--do_split_k -use_adaptive_sampling -save_test_potentials
+-do_split_k -use_adaptive_sampling -save_test_potentials -do_test_evaluation
+#-training_potentials_file None \
 
-time ibrun tacc-affinity /home1/02497/jll2752/lib/askit_skx/treecode/src/askit_distributed_main.exe \
+time ibrun tacc-affinity ${ASKIT_DIR}/src/askit_distributed_main.exe \
 -data ${trnfile} \
 -knn_file ${knnfile} \
 -charges ${ppvfile} -N ${n_ref} -d ${FKEEP} -k ${KCUT} -m ${mm} -id_rank ${id_rank} -min_skeleton_level 7 -id_tol ${id_tol} \
 -sort_method nn -kernel_type gaussian -balloon_k 0 -h ${BW} -c 0 -p 0 -output output.out \
--training_potentials_file None \
 -test_potentials_file ${yyfile} -num_power_iterations 10 \
 -test_data_file ${tstfile} \
 -test_knn_file ${tknnfile} \
 -num_test_points ${n_tst} -num_skel_targets 2 -num_uniform_required 0 -oversampling_fac 5 -err 100 \
 -num_error_repeats 10 -max_sampling_iterations 3 -adaptive_sample_size 2 -pruning_num_neighbors 0 \
 -neighbors_to_pass_up 4 -lambda 0.0 -binary -do_simplified_adaptive_rank \
--do_split_k -use_adaptive_sampling -save_test_potentials
+-do_split_k -use_adaptive_sampling -save_test_potentials -do_test_evaluation
+#-training_potentials_file None \
 
 
-time ibrun tacc-affinity /home1/02497/jll2752/lib/askit_skx/treecode/src/askit_distributed_main.exe \
+time ibrun tacc-affinity ${ASKIT_DIR}/src/askit_distributed_main.exe \
 -data ${trnfile} \
 -knn_file ${knnfile} \
 -charges ${cmfilein} -N ${n_ref} -d ${FKEEP} -k ${KCUT} -m ${mm} -id_rank ${id_rank} -min_skeleton_level 7 -id_tol ${id_tol} \
@@ -102,9 +110,9 @@ time ibrun tacc-affinity /home1/02497/jll2752/lib/askit_skx/treecode/src/askit_d
 -num_test_points ${n_tst} -num_skel_targets 2 -num_uniform_required 0 -oversampling_fac 5 -err 100 \
 -num_error_repeats 10 -max_sampling_iterations 3 -adaptive_sample_size 2 -pruning_num_neighbors 0 \
 -neighbors_to_pass_up 4 -lambda 0.0 -binary -do_simplified_adaptive_rank \
--do_split_k -use_adaptive_sampling -save_test_potentials
+-do_split_k -use_adaptive_sampling -save_test_potentials -do_test_evaluation
 
-time ibrun tacc-affinity /home1/02497/jll2752/lib/askit_skx/treecode/src/askit_distributed_main.exe \
+time ibrun tacc-affinity ${ASKIT_DIR}/src/askit_distributed_main.exe \
 -data ${trnfile} \
 -knn_file ${knnfile} \
 -charges ${rofilein} -N ${n_ref} -d ${FKEEP} -k ${KCUT} -m ${mm} -id_rank ${id_rank} -min_skeleton_level 7 -id_tol ${id_tol} \
@@ -116,14 +124,19 @@ time ibrun tacc-affinity /home1/02497/jll2752/lib/askit_skx/treecode/src/askit_d
 -num_test_points ${n_tst} -num_skel_targets 2 -num_uniform_required 0 -oversampling_fac 5 -err 100 \
 -num_error_repeats 10 -max_sampling_iterations 3 -adaptive_sample_size 2 -pruning_num_neighbors 0 \
 -neighbors_to_pass_up 4 -lambda 0.0 -binary -do_simplified_adaptive_rank \
--do_split_k -use_adaptive_sampling -save_test_potentials
+-do_split_k -use_adaptive_sampling -save_test_potentials -do_test_evaluation
 " > ${filen}.job
 
 # If test, cat file, o.w. sbatch and rm
 if [ ! $TEST ]
 then
-    sbatch ${filen}.job 
-    rm ${filen}.job
+    if [ -z "$JID" ]
+    then
+        sbatch ${filen}.job
+    else
+        sbatch --dependency=afterok:${JID} ${filen}.job
+    fi
+    #rm ${filen}.job
 else
     cat ${filen}.job
 fi
