7d94a1ab55b25d28a12951661442bb24a0144e08
Sameer Tharakan <tharakan@login2.stampede2.tacc.utexas.edu>
2018-06-08 11:27:10 -0500

    changed to print function val, instead of diff

--- matlab/tools/PrintIterationInfo.m
+++ matlab/tools/PrintIterationInfo.m
@@ -1,16 +1,16 @@
-function [  ] = PrintIterationInfo( ii,f0,g0,fc,gc,qoi )
+function [  ] = PrintIterationInfo( ii,g0,fc,gc,qoi )
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
 
 if nargin == 0
     % print header
-    fprintf(' Iter |  fc - f0  |  gc/g0  |  qoi  |\n');
+    fprintf(' Iter |    f_c    |  gc/g0  |  qoi  |\n');
     fprintf('-------------------------------------\n');
 else
     % print actual data
     grel = norm(gc,'fro')/norm(g0,'fro');
-    fprintf(' %4d | %-9.4g | %-7.4g | %-5.3g | \n',ii,fc - f0,grel,qoi);
+    fprintf(' %4d | %-9.4g | %-7.4g | %-5.3g | \n',ii,fc,grel,qoi);
 end
 end
 
