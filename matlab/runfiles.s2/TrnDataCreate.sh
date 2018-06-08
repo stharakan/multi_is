caf1c1f4713d57238d8a69d1c065915e64e8e445
Sameer Tharakan <tharakan@login2.stampede2.tacc.utexas.edu>
2018-06-08 11:23:26 -0500

    changed to use knl nodes by default

--- matlab/runfiles.s2/TrnDataCreate.sh
+++ matlab/runfiles.s2/TrnDataCreate.sh
@@ -10,10 +10,10 @@ echo "#!/bin/bash
 #SBATCH -J ${filen} # Job name
 #SBATCH -o ${filen}.o # Name of stdout output file
 #SBATCH -e ${filen}.e # Name of stderr error file
-#SBATCH -p skx-normal # Queue name
+#SBATCH -p normal # Queue name
 #SBATCH -N 1 # Total # of nodes (now required)
-#SBATCH -n 20 # Total # of mpi tasks
-#SBATCH -t 12:00:00 # Run time (hh:mm:ss)
+#SBATCH -n 1 # Total # of mpi tasks
+#SBATCH -t 5:00:00 # Run time (hh:mm:ss)
 #SBATCH --mail-user=sameer@ices.utexas.edu
 ##SBATCH --mail-type=begin # Send email at begin and end of job
 ##SBATCH --mail-type=end # Send email at begin and end of job
