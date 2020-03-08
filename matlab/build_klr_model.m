947d9415fd59093032ab436717493e37dc397bfa
Sameer Tharakan <tharakan@login1.ls5.tacc.utexas.edu>
2020-03-08 16:38:50 -0500

    pushing from ls5 -- updates to run and keep variables in a saved file

--- matlab/build_klr_model.m
+++ matlab/build_klr_model.m
@@ -1,9 +1,10 @@
 % Variables
-klr_dir = '/Users/stharakan/Documents/rklr/matlab/';
-data_file = 'test.mat';
-rank = 32;
+data_locations;
+rank = 1024 
+sigma = 68.7
+lambda = 9.0E-7
+batches = 4
 sigma_tol = [0.1];
-save_file = './klr_mdoel.mat';
 
 % klr options
 options.tol_meth = 'tst';
@@ -17,19 +18,26 @@ options.outer_its = 5;
 addpath(genpath(klr_dir));
 
 % Load training data
-data = load(data_file);
+load(data_file);
+clear trn_list val_list tst_list 
+data.Xtrain = Xtrain;
+data.Ytrain = Ytrain;
+data.Xtest = Xtest;
+data.Ytest = Ytest;
 
 % Select bandwidth
-%[sigma,err] = find_sigmas(data.Xtrain,rank,sigma_tol);
+size(data.Xtrain)
+size(data.Xtest)
+%[sigma,err] = find_sigmas(data.Xtrain,512,sigma_tol);
 %fprintf('Sigma for %s with tolerance miss of %5.3f is %5.3f\n',data_file,err,sigma );
-sigma = 0.5;
 
 % Select reg
-[lambda,pcorr] = find_lambda(data.Xtrain,data.Ytrain,sigma,rank);
-fprintf('Lambda for %s with sigma %5.3f is %5.3f\n',data_file,sigma,lambda );
+%[lambda,pcorr] = find_lambda(data.Xtrain,data.Ytrain,sigma,rank);
+%fprintf('Lambda for %s with sigma %5.3f is %5.3f\n',data_file,sigma,lambda );
 
 % Get KA
-KA = OneShot(data.Xtrain,data.Ytrain,rank,rank,sigma);
+%KA = OneShot(data.Xtrain,data.Ytrain,rank,rank,sigma);
+KA = EnsNyst(data.Xtrain,data.Ytrain,rank,rank,sigma,batches);
     kerr = KA.matvec_errors(10);
     disp(['Decomp err ', num2str(kerr)]);
     disp('---------------------------------');
@@ -39,4 +47,4 @@ klr = rklr(KA,data,lambda,[],options);
 T = klr.AssembleTable();
 
 % Save
-save(save_file,'klr','sigma','lambda');
+save(klr_file,'klr','sigma','lambda','T', '-v7.3');
