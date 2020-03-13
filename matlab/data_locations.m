136ba85b524b625ab1d0def783ea5fd8d5b92090
Sameer Tharakan <sameer.tharakan@gmail.com>
2020-03-13 13:48:21 -0500

    made a data directory locally to keep things clean

--- matlab/data_locations.m
+++ matlab/data_locations.m
@@ -3,7 +3,7 @@ if 0
     bdir_dnn_tissue = '/Users/stharakan/Documents/brain_data/dnn/brats_val_tissue_nvidia_unet_softmax/';
     bdir = '/Users/stharakan/Documents/brain_data/brats18/ALL/';
     klr_dir = '/Users/stharakan/Documents/rklr/matlab/';
-    save_dir = './';
+    save_dir = './data/';
     bcell = {'Brats18_2013_23_1','Brats18_2013_25_1','Brats18_2013_5_1','Brats18_CBICA_AQN_1','Brats18_CBICA_AOZ_1'};
     
     
