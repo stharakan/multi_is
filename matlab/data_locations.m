69b22666d44f126fee81e0f9053b5d491e3374a3
Sameer Tharakan <sameer.tharakan@gmail.com>
2020-03-13 13:42:27 -0500

    changed starting files

--- matlab/data_locations.m
+++ matlab/data_locations.m
@@ -1,20 +1,23 @@
 if 0
-bdir_dnn_tumor = '/Users/stharakan/Documents/brain_data/dnn/brats_val_tumor_nvidia_unet_sigmoid/';
-bdir_dnn_tissue = '/Users/stharakan/Documents/brain_data/dnn/brats_val_tissue_nvidia_unet_softmax/';
-bdir = '/Users/stharakan/Documents/brain_data/brats18/ALL/';
-
-bcell = {'Brats18_2013_23_1','Brats18_2013_25_1','Brats18_2013_5_1','Brats18_CBICA_AQN_1','Brats18_CBICA_AOZ_1'};
+    bdir_dnn_tumor = '/Users/stharakan/Documents/brain_data/dnn/brats_val_tumor_nvidia_unet_sigmoid/';
+    bdir_dnn_tissue = '/Users/stharakan/Documents/brain_data/dnn/brats_val_tissue_nvidia_unet_softmax/';
+    bdir = '/Users/stharakan/Documents/brain_data/brats18/ALL/';
+    klr_dir = '/Users/stharakan/Documents/rklr/matlab/';
+    save_dir = './';
+    bcell = {'Brats18_2013_23_1','Brats18_2013_25_1','Brats18_2013_5_1','Brats18_CBICA_AQN_1','Brats18_CBICA_AOZ_1'};
+    
+    
+else
+    bdir_dnn_tumor =  '/scratch/03158/tharakan/brats_val_tumor_nvidia_unet_sigmoid/';
+    bdir_dnn_tissue = '/scratch/03158/tharakan/brats_val_tissue_nvidia_unet_softmax/';
+    bdir =            '/scratch/03158/tharakan/brats18/ALL/';
+    
+    klr_dir = '/work/03158/tharakan/research/rklr/matlab/';
+    save_dir = '/scratch/03158/tharakan/miccai20/';
 end
 
-bdir_dnn_tumor =  '/scratch/03158/tharakan/brats_val_tumor_nvidia_unet_sigmoid/';
-bdir_dnn_tissue = '/scratch/03158/tharakan/brats_val_tissue_nvidia_unet_softmax/';
-bdir =            '/scratch/03158/tharakan/brats18/ALL/';
-
-klr_dir = '/work/03158/tharakan/research/rklr/matlab/';
-addpath(genpath(klr_dir));
 
 % things to save
-data_file = '/scratch/03158/tharakan/miccai20/dnn_features.mat';
-save_dir = '/scratch/03158/tharakan/miccai20/rank4096/';
-klr_file = [save_dir,'klr_model.mat'];
+klr_model_dir = [save_dir,'/klr_model_files/'];
+data_file = [save_dir,'dnn_features.mat'];
 single_brain_file = [save_dir,'single_brain_results.mat'];
--- matlab/startup.m
+++ matlab/startup.m
@@ -1,3 +1,5 @@
 addpath( genpath('./') );
+data_locations;
+addpath(genpath(klr_dir));
 %addpath([ getenv('HOME'),'/lib/liblinear-2.20/matlab/']);
 
