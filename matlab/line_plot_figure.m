1ef96c1e06fdec1b4db39a665acdaa7aaa9bfccf
Sameer Tharakan <sameer.tharakan@gmail.com>
2020-04-02 19:12:57 -0400

    made line plot figure plot is variances rather than probabilities

--- matlab/line_plot_figure.m
+++ matlab/line_plot_figure.m
@@ -21,39 +21,39 @@ xx = 1:size(seg,sum_dim);
 seg_line = get_1D_line(seg,dim,idx);
 
 % prep dnn probs
-dnn_probs = permute(dnn_probs,[1,2,4,3]);
 dnn_sum = sum(dnn_probs,3);
 dnn_sum(dnn_sum == 0) = 1.0;
 dnn_probs = bsxfun(@rdivide,dnn_probs,dnn_sum);
 
 % prepare is probs
 is_probs_means = mean(is_probs,3);
-is_probs_stds  = std(is_probs,0,3);
+is_probs_var  = var(is_probs,0,3);
 is_probs_means = reshape(is_probs_means,size(seg,1),size(seg,2),[]);
-is_probs_stds = reshape(is_probs_stds,size(seg,1),size(seg,2),[]);
-wt_stds = sqrt(sum( is_probs_stds(:,:,2:4).^2,3 )) ;
+is_probs_var = reshape(is_probs_var,size(seg,1),size(seg,2),[]);
+wt_stds = sqrt(sum( is_probs_var(:,:,2:4).^2,3 )) ;
 
 % initialize figure axes
 ha = tight_subplot(3,1,[.05,.01],.05,.05);
 
 % wt figure
+% old = sum(is_probs_stds(:,:,2:4),3)
 yy = seg_line ~= 0;
 [dnn_line,klr_line,is_line] = get_all_1D_lines(dim,idx,sum(dnn_probs,3),...
-    sum(klr_probs(:,:,2:4),3),sum(is_probs_means(:,:,2:4),3));
+    sum(klr_probs(:,:,2:4),3),sum(is_probs_var(:,:,2:4),3));
 plot_1D_lines(xx,yy,dnn_line, klr_line,...
     is_line,ha(1),'Whole tumor')
 
 % edema fig
 yy = seg_line == 2;
 [dnn_line,klr_line,is_line] = get_all_1D_lines(dim,idx,dnn_probs(:,:,2),...
-    klr_probs(:,:,3),is_probs_means(:,:,3));
+    klr_probs(:,:,3),is_probs_var(:,:,3));
 plot_1D_lines(xx,yy,dnn_line, klr_line,...
     is_line,ha(2),'Edema')
 
 % en fig
 yy = seg_line == 4;
 [dnn_line,klr_line,is_line] = get_all_1D_lines(dim,idx,dnn_probs(:,:,3),...
-    klr_probs(:,:,4),is_probs_means(:,:,4));
+    klr_probs(:,:,4),is_probs_var(:,:,4));
 plot_1D_lines(xx,yy,dnn_line, klr_line,...
     is_line,ha(3),'Enhancing')
 
@@ -73,7 +73,7 @@ plot(dnn_line); hold on
 plot(klr_line); hold on
 plot(is_line); 
 grid on
-legend('Truth','DNN','KLR','KLR-IS')
+legend('Truth','DNN','KLR','IS-var')
 title(title_str)
 
 end
